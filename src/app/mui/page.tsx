5225de9e87554cf6ba2742259f9d73a1881893cd
shahab.ramzan <shahab.ramzan@codedistrict.com>
2023-12-04 16:08:46 +0500

    build issue fixed

--- src/app/mui/page.tsx
+++ src/app/mui/page.tsx
@@ -2,13 +2,10 @@ import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-import { useRef, useState } from "react";
 
 import RecipeReviewCard from "@/components/card";
 
 export default function Home() {
-  const ref = useRef();
-  const [test, setTest] = useState("Hello");
   return (
     <main>
       <Container>
