be16a7837bacc17de1b433a02801f78c416f8767
shahab.ramzan <shahab.ramzan@codedistrict.com>
2023-12-04 17:03:38 +0500

    progressbar removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 // import './globals.css'
 import ThemeRegistry from "../lib/muiRegistry";
 import { Providers } from "../store/storeProvider";
-import ProgressBarProvider from "@/components/progressBarProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,9 +32,7 @@ export default function RootLayout({
               <Link href="/mui">Mui</Link>
               <Link href="/ag-grid">AG Grid</Link>
             </div>
-            <Providers>
-              <ProgressBarProvider>{children}</ProgressBarProvider>
-            </Providers>
+            <Providers>{children}</Providers>
           </>
         </ThemeRegistry>
       </body>
