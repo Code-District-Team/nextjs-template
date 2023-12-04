7135ef40bab7a3965e49cbac0e8fc44c55e9b32d
shahab.ramzan <shahab.ramzan@codedistrict.com>
2023-12-04 17:51:26 +0500

    next-translate integrated for Internationalization

--- src/app/mui/page.tsx
+++ src/app/mui/page.tsx
@@ -2,13 +2,17 @@ import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import useTranslation from "next-translate/useTranslation";
 
 import RecipeReviewCard from "@/components/card";
 
 export default function Home() {
+  const { t } = useTranslation("common");
+
   return (
     <main>
       <Container>
+        <p>{t("about")}</p>
         <Box>
           <Card>
             <Typography variant="h2">Hello World ~</Typography>
