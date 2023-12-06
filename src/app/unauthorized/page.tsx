acbdeaccd3ffb3d8d14886fbf687f6b9390666dc
Usama Mehmood <usama.mehmood@codedistrict.com>
2023-12-06 19:38:41 +0500

    Added stack

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, Paper, Typography } from '@mui/material';
+import { Button, Divider, Paper, Stack, Typography } from '@mui/material';
 import { useRouter } from 'next/navigation'
 
 export let router: any = null;
@@ -8,19 +8,26 @@ const ForbiddenMessage = () => {
   router = useRouter();
 
   return (
-    <Paper elevation={3} style={{ padding: '20px', textAlign: 'center' }}>
+    <Paper elevation={3} style={{ margin: '20px', padding: '20px', textAlign: 'center' }}>
       <Typography variant="h5" gutterBottom>
         Forbidden Access!
       </Typography>
       <Typography variant="subtitle1" gutterBottom>
         You do not have permission to access this resource.
       </Typography>
-      <Button variant="contained" color="secondary">
-        Go Back
-      </Button>
-      <Button variant="outlined" color="secondary">
-        Try Again
-      </Button>
+      <Stack
+        direction="row"
+        spacing={2}
+        justifyContent="center"
+      >
+        <Button variant="contained" color="secondary">
+          Go Home
+        </Button>
+        <Button variant="outlined" color="secondary">
+          Try Again
+        </Button>
+      </Stack>
+
     </Paper>
   );
 };
