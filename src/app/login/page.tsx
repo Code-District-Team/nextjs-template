f5516d6a4618b8e5a8c87bd45e375808a575fd6a
AwaisYaseen <34713534+AwaisYaseen@users.noreply.github.com>
2023-12-07 15:41:31 +0500

    amplify integrated

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,23 +1,29 @@
 "use client";
+import '@aws-amplify/ui-react/styles.css';
+
+import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
 import { deleteCookies, getSingleCookie } from "@/lib/cookies";
 import { userLoginApi } from "@/network/api/user";
-
-const Login = () => {
+const Login = ({signOut, user}: WithAuthenticatorProps) => {
   const handleLogin = async () => {
     const userCreds = {
       username: "kminchelle",
       password: "0lelplR",
     };
     await userLoginApi(userCreds);
+    
     console.log("first");
   };
-  const user = getSingleCookie("auth");
+  // const user = getSingleCookie("auth");
   return (
     <>
       <button onClick={() => handleLogin()}>Login</button>
       <button onClick={() => deleteCookies("auth")}>Clear cookies</button>
+
     </>
   );
 };
 
-export default Login;
+export default withAuthenticator(Login);
