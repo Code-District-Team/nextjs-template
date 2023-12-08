7b74794d000df03811b110ea8f85f472b73c5f1f
shahab.ramzan <shahab.ramzan@codedistrict.com>
2023-12-08 12:48:39 +0500

    post request type added

--- src/network/requests/posts.ts
+++ src/network/requests/posts.ts
@@ -1,25 +1,14 @@
 import K from '../../constants';
-import { store } from '../../store';
 import Request from '.';
 
 export default class PostRequest extends Request {
-  constructor(params: any) {
-    super(params);
-  }
-
-  // Define request functions below.
-
-  static getAllPosts() {
-    let state = store.getState();
-    // const { auth } = state;
-
+  static getAllPosts(): Request {
     return new Request(
       K.Network.URL.posts,
       K.Network.Method.GET,
       null,
       K.Network.Header.Type.Json,
       {}
-      // auth.loggedInUser?.token
     );
   }
 }
