cfd5604ac7a464a084f88d5b293d2f5568e85820
shahab.ramzan <shahab.ramzan@codedistrict.com>
2023-12-08 17:48:50 +0500

    bash files added

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   '**/*.(js|jsx|ts|tsx)': filenames => [
     // `yarn lint ${filenames.join(' ')}`,
+    'yarn build',
     `yarn format ${filenames.join(' ')}`,
     'git add src/',
   ],
