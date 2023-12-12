62d2d6b8bee15b8b9fc768b642d8c2e89403815d
shahab.ramzan <shahab.ramzan@codedistrict.com>
2023-12-12 17:53:40 +0500

    new relic configured

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ const protectedRoutes: string[] = ['/mui', '/posts'];
 export default async function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
   if (!isUserAuthenticated() && protectedRoutes.includes(pathname)) {
-    const absoluteURL = new URL('/', req.nextUrl.origin);
+    const absoluteURL = new URL('/unauthorized', req.nextUrl.origin);
     return NextResponse.redirect(absoluteURL.toString());
   }
 }
