bc0b2f2021c40c57ed172a83904e60dc7bf5ec6e
shahab.ramzan <shahab.ramzan@codedistrict.com>
2023-12-14 12:12:12 +0500

    dynamic meta added

--- src/app/mui/page.tsx
+++ src/app/mui/page.tsx
@@ -2,6 +2,7 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import { Metadata } from 'next';
 import useTranslation from 'next-translate/useTranslation';
 
 import RecipeReviewCard from '@/components/card';
@@ -25,3 +26,10 @@ export default function Home() {
     </main>
   );
 }
+
+export const metadata: Metadata = {
+  title: {
+    absolute: 'login Page',
+  },
+  description: 'This is the login',
+};
